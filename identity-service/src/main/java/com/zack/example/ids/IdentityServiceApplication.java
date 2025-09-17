f617ab7d81842c4b9f04b854d415fc54d63ca75f
h3yzack <zuhaimia@gmail.com>
2025-09-17 06:11:03 +0000

    Initial project commit

---
+++ identity-service/src/main/java/com/zack/example/ids/IdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zack.example.ids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApplication.class, args);
+	}
+
+}
